a691fa91807371a28cca246d44286e6ac0448500
Ulukbek <l9l9ndos@gmail.com>
2023-09-02 03:02:59 +0600

    Last commit maybe

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from aiogram import Bot, Dispatcher, types, executor
 from config import TOKEN
 import pyjokes
+import randfacts
 from googletrans import Translator
 
 
@@ -12,11 +13,13 @@ translator = Translator()
 keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
 btn1 = types.KeyboardButton(text='Анекдот')
 keyboard.add(btn1)
+btn2 = types.KeyboardButton(text='Случайный факт')
+keyboard.add(btn2)
 
 
 @dp.message_handler(commands=['start'])
 async def cmd_start(message: types.Message):
-    await message.answer('Привет!\nЯ умею отправлять анекдоты!', reply_markup=keyboard)
+    await message.answer('Привет!\nЯ умею отправлять анекдоты и\nслучайные факты!', reply_markup=keyboard)
 
 
 @dp.message_handler(text='Анекдот')
@@ -24,6 +27,16 @@ async def cmd_start(message: types.Message):
     joke = pyjokes.get_joke()
     translation = translator.translate(joke, dest='ru')
     await message.answer(translation.text)
+
+
+@dp.message_handler(text='Случайный факт')
+async def cmd_start(message: types.Message):
+    randfact = randfacts.get_fact(False)
+    translation = translator.translate(randfact, dest='ru')
+    await message.answer(translation.text)
+
+
+
 
 
 if __name__ == '__main__':
